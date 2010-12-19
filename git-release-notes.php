735046546f9dfa36c0a4405a82b4ad2908ac9a9e
Josh Oldenburg <josh.legogeek@gmail.com>
2010-12-19 15:16:56 -0500

    Fixed example.

--- git-release-notes.php
+++ git-release-notes.php
@@ -69,9 +69,9 @@ function usage($msg = NULL) {
     print "ERROR: $msg\n";
   }
   print <<<EOF
-Usage: $argv[0] [previous_release_tag] [current_release_tag]
+Usage: $argv[0] previous_release_tag current_release_tag [path/to/git/binary]
 For example:
-$argv[0] 6.x-1.0 6.x-1.1
+$argv[0] 6.x-1.0 6.x-1.1 /usr/local/git/bin/git
 
 EOF;
   exit(empty($msg) ? 0 : 1);
